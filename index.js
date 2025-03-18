117cf07e3888e69d4348255e74b7f89f47523a56
Mercy Muriithi <mercywmuriithi.mm@gmail.com>
2025-03-18 13:53:43 +0300

    string methods

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser = "GRACE HOPPER";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage =
+  "Welcome to Flatbook, ".toUpperCase() + currentUser + "!";
+
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
